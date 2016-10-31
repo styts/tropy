17302dbb151f5b0953015c22eb13b16ce1f46bfe
Johannes Krtek <johannes.krtek@flachware.com>
2016-10-31 20:57:53 +0100

    Fix errors.

--- src/components/project.js
+++ src/components/project.js
@@ -1,20 +1,19 @@
 'use strict'
 
 const React = require('react')
-const { PropTypes } = React
 
 const sidebarWidth = { width: '250px' }
 
-const Project = ({ }) => (
+const Project = () => (
   <div id="project">
     <div id="project-view">
       <div style={sidebarWidth}>
         <div id="sidebar">
-          <div className="sidebar-body"></div>
+          <div className="sidebar-body"/>
         </div>
       </div>
     </div>
-    <div id="item"></div>
+    <div id="item"/>
   </div>
 )
 
