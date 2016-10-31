c415307b79d35e941e55ac8334c0fea63a445bc6
Johannes Krtek <johannes.krtek@flachware.com>
2016-10-31 20:09:29 +0100

    Debug.

--- src/components/project.js
+++ src/components/project.js
@@ -3,52 +3,22 @@
 const React = require('react')
 const { PropTypes } = React
 const { connect } = require('react-redux')
-const { ProjectSidebar } = require('./project/sidebar')
-const { Items } = require('./items')
-const { PanelGroup } = require('./panelgroup')
-const { Viewer } = require('./viewer')
 const { noop } = require('../common/util')
-const { context } = require('../actions/ui')
 
 const sidebarWidth = { width: '250px' }
 const panelWidth = { width: '320px' }
 
-const Project = ({ showContextMenu }) => (
-  <div id="project" onContextMenu={showContextMenu}>
+const Project = ({ }) => (
+  <div id="project">
     <div id="project-view">
-      <div className="resizable" style={sidebarWidth}>
-        <ProjectSidebar toolbar={ARGS.frameless}/>
-        <div className="resizable-handle-col resizable-handle-right"/>
+      <div className="resizable">
+        <div id="sidebar">
+          <div className="sidebar-body"></div>
+        </div>
       </div>
-      <main>
-        <Items/>
-      </main>
     </div>
-    <section id="item">
-      <div className="resizable" style={panelWidth}>
-        <PanelGroup/>
-        <div className="resizable-handle-col resizable-handle-left"/>
-      </div>
-      <Viewer/>
-    </section>
+    <div id="item"></div>
   </div>
 )
 
-Project.propTypes = {
-  showContextMenu: PropTypes.func
-}
-
-Project.defaultProps = {
-  showContextMenu: noop
-}
-
-module.exports = {
-  Project: connect(
-    null,
-    dispatch => ({
-      showContextMenu(event) {
-        dispatch(context.show(event))
-      }
-    })
-  )(Project)
-}
+module.exports = { Project }
