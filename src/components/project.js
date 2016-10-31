153840f934601ea3aaac788a34777ea59f0bc35a
Johannes Krtek <johannes.krtek@flachware.com>
2016-10-31 21:02:36 +0100

    Fix error.

--- src/components/project.js
+++ src/components/project.js
@@ -1,11 +1,16 @@
 'use strict'
 
 const React = require('react')
+const { PropTypes } = React
+const { connect } = require('react-redux')
+const { noop } = require('../common/util')
+const { context } = require('../actions/ui')
+
 
 const sidebarWidth = { width: '250px' }
 
-const Project = () => (
-  <div id="project">
+const Project = ({ showContextMenu }) => (
+  <div id="project" onContextMenu={showContextMenu}>
     <div id="project-view">
       <div style={sidebarWidth}>
         <div id="sidebar">
@@ -17,4 +22,22 @@ const Project = () => (
   </div>
 )
 
-module.exports = { Project }
+
+Project.propTypes = {
+  showContextMenu: PropTypes.func
+}
+
+Project.defaultProps = {
+  showContextMenu: noop
+}
+
+module.exports = {
+  Project: connect(
+    null,
+    dispatch => ({
+      showContextMenu(event) {
+        dispatch(context.show(event))
+      }
+    })
+  )(Project)
+}
