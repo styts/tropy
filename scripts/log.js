490dea013af02e6396aeeba75c72b905e62d1d45
Kirill Stytsenko <kirill@styts.com>
2017-04-25 19:54:19 +0200

    Remove winston dependency in scripts
    Log with `console.log` instead

--- scripts/log.js
+++ scripts/log.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const winston = require('winston')
+/* eslint-disable no-console */
 
 const colors = require('colors/safe')
 const pad = require('string.prototype.padstart')
@@ -11,16 +11,31 @@ function colorize(level, text) {
   return colors[COLOR[level] || 'gray'](text || level)
 }
 
+function importantLevel(level) {
+  level = level.toLowerCase()
+  return level in COLOR
+}
+
 function prefix(options) {
+  options.meta = options.meta || {}
+  if (!options.meta.tag && importantLevel(options.level)) {
+    // we need some text to colorize if we have a non-info
+    options.meta.tag = options.level.toUpperCase()
+  }
   return colorize(options.level, pad(options.meta.tag || '', 12, ' '))
 }
 
-module.exports = new winston.Logger({
-  transports: [
-    new winston.transports.Console({
-      formatter(options) {
-        return `${prefix(options)} ${options.message}`
-      }
-    })
-  ]
-})
+
+function log(level, message, meta) {
+  let options = { meta }
+  options.level = level
+  console.log(prefix(options), message)
+}
+
+const logger = {
+  info: (...args) => log('info', ...args),
+  warn: (...args) => log('warn', ...args),
+  error: (...args) => log('error', ...args)
+}
+
+module.exports = logger
